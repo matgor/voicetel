ec5bb3c06f88783572c5f45374c21a5987f876e9
matgor <mateusz.gorczyca@gmail.com>
2018-05-26 13:04:06 +0200

    added table, sorter, filter and clear filter

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+// TABLE
+
+$("#phone-table").tabulator({
+    // height: 800,
+    columns: [
+        { title: "Imie", field: "imie", sortable: true },
+        { title: "Nazwisko", field: "nazwisko", sortable: true },
+        { title: "Forward", field: "forward", sortable: true },
+        { title: "Number", field: "number", sortable: true, sorter: "number" },
+        { title: "Dodano", field: "dodano", sortable: true },
+    ],
+});
+
+// API REQUEST
+$("#phone-table").tabulator("setData", "http://esense2.voicetel.pl:20005/getResults/", { firstName: "voicetel", lastName: "test" });
+
+//FILTER
+$("#filter-button").click(function() {
+    $("#phone-table").tabulator("setFilter", "forward", "=", "yes");
+});
+
+//CLEAR FILTER
+
+$("#clearFilter-button").click(function() {
+    $("#phone-table").tabulator("clearFilter");
+});
